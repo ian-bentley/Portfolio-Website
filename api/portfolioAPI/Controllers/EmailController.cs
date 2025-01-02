e634a98330c18f98a854de29382643821625ef5b
Ian Bentley <ibentley981203@gmail.com>
2025-01-01 17:52:58 -0800

    adds contact email response
    - Adds ContactMe http post
    - Sends email through service that contains the post

---
+++ api/portfolioAPI/Controllers/EmailController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using portfolioAPI.Models;
+using portfolioAPI.Services.EmailService;
+
+namespace portfolioAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private readonly IEmailService _emailService;
+        private readonly IConfiguration _configuration;
+
+        public EmailController(IEmailService emailService, IConfiguration configuration)
+        {
+            _emailService = emailService;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("ContactMe")]
+        [Consumes("application/json")]
+        public IActionResult ContactMe([FromBody] Email email)
+        {
+            email.To = _configuration.GetValue<string>("EmailServiceConfig:WebsiteEmail");
+            _emailService.SendEmailAsync(email);
+            JsonResult jsonResult = new JsonResult("Contact Email Sent");
+            return Ok(jsonResult);
+        }
+    }
+}
